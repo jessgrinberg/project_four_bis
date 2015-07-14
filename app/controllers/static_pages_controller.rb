dfe224e58eab6d1f0e8a3c2dc44d6de609287b5d
Jessica Grinberg <jessgrinberg@gmail.com>
2015-07-13 19:56:06 -0700

    adding css all elements on same page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,14 @@
 class StaticPagesController < ApplicationController
   def welcome
+
+    @maps = Map.all
+        respond_to do |format|
+        format.html {
+            render
+        }
+        format.json {
+            render json: @maps
+        }
+  end
   end
 end
